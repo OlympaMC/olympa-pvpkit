6907df77deeb5a47f052e719a5dae17f4be3d73b
Tristiisch <tristiisch74@hotmail.com>
2021-06-10 23:16:31 +0200

    fix to api

--- src/main/java/fr/olympa/pvpkit/CustomWorldNBTStorage.java
+++ src/main/java/fr/olympa/pvpkit/CustomWorldNBTStorage.java
@@ -8,15 +8,15 @@ import org.bukkit.persistence.PersistentDataType;
 
 import com.mojang.datafixers.DataFixer;
 
-import fr.olympa.api.common.provider.AccountProvider;
+import fr.olympa.api.common.provider.AccountProviderAPI;
 import net.minecraft.server.v1_16_R3.Convertable.ConversionSession;
 import net.minecraft.server.v1_16_R3.EntityHuman;
 import net.minecraft.server.v1_16_R3.WorldNBTStorage;
 
 public class CustomWorldNBTStorage extends WorldNBTStorage {
-	
+
 	public static final NamespacedKey PLAYER_KIT = new NamespacedKey(OlympaPvPKit.getInstance(), "pvpkit_kit");
-	
+
 	public CustomWorldNBTStorage(ConversionSession convertable_conversionsession, DataFixer datafixer) throws ReflectiveOperationException {
 		super(convertable_conversionsession, datafixer);
 		File playerDir = new File(OlympaPvPKit.getInstance().getDataFolder(), "playerdata");
@@ -25,21 +25,24 @@ public class CustomWorldNBTStorage extends WorldNBTStorage {
 		field.setAccessible(true);
 		field.set(this, playerDir);
 	}
-	
+
 	@Override
 	public void save(EntityHuman entityhuman) {
-		OlympaPlayerPvPKit player = AccountProvider.getter().get(entityhuman.getUniqueID());
-		if (player == null) return; // appelé après que le joueur soit enlevé du cache
+		OlympaPlayerPvPKit player = AccountProviderAPI.getter().get(entityhuman.getUniqueID());
+		if (player == null)
+			return; // appelé après que le joueur soit enlevé du cache
 		if (player.isInPvPZone()) {
 			entityhuman.getBukkitEntity().getPersistentDataContainer().set(PLAYER_KIT, PersistentDataType.STRING, player.getUsedKit().getId());
 			OlympaPvPKit.getInstance().sendMessage("Sauvegarde du joueur %s avec le kit %s dans %s.", entityhuman.getName(), player.getUsedKit().getId(), getPlayerDir().getPath());
 			super.save(entityhuman);
-		}else {
+		} else {
 			File file = new File(getPlayerDir(), entityhuman.getUniqueIDString() + ".dat");
-			if (file.exists()) file.delete();
+			if (file.exists())
+				file.delete();
 			file = new File(getPlayerDir(), entityhuman.getUniqueIDString() + ".dat_old");
-			if (file.exists()) file.delete();
+			if (file.exists())
+				file.delete();
 		}
 	}
-	
+
 }
--- src/main/java/fr/olympa/pvpkit/OlympaPlayerPvPKit.java
+++ src/main/java/fr/olympa/pvpkit/OlympaPlayerPvPKit.java
@@ -9,41 +9,41 @@ import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
-import fr.olympa.api.spigot.economy.MoneyPlayerInterface;
-import fr.olympa.api.spigot.economy.OlympaMoney;
-import fr.olympa.api.common.provider.AccountProvider;
+import fr.olympa.api.common.observable.ObservableInt;
+import fr.olympa.api.common.provider.AccountProviderAPI;
 import fr.olympa.api.common.provider.OlympaPlayerObject;
 import fr.olympa.api.common.sql.SQLColumn;
-import fr.olympa.api.common.observable.ObservableInt;
+import fr.olympa.api.spigot.economy.MoneyPlayerInterface;
+import fr.olympa.api.spigot.economy.OlympaMoney;
 import fr.olympa.core.spigot.OlympaCore;
 import fr.olympa.pvpkit.kits.Kit;
 import fr.olympa.pvpkit.xp.LevelManagement;
 import fr.olympa.pvpkit.xp.XPManagement;
 
 public class OlympaPlayerPvPKit extends OlympaPlayerObject implements MoneyPlayerInterface {
-	
+
 	private static final SQLColumn<OlympaPlayerPvPKit> COLUMN_MONEY = new SQLColumn<OlympaPlayerPvPKit>("money", "DOUBLE NULL DEFAULT 0", Types.DOUBLE).setUpdatable();
 	private static final SQLColumn<OlympaPlayerPvPKit> COLUMN_KILL_STREAK = new SQLColumn<OlympaPlayerPvPKit>("kill_streak", "SMALLINT UNSIGNED NULL DEFAULT 0", Types.SMALLINT).setUpdatable();
 	private static final SQLColumn<OlympaPlayerPvPKit> COLUMN_KILL_STREAK_MAX = new SQLColumn<OlympaPlayerPvPKit>("kill_streak_max", "SMALLINT UNSIGNED NULL DEFAULT 0", Types.SMALLINT).setUpdatable();
 	private static final SQLColumn<OlympaPlayerPvPKit> COLUMN_LEVEL = new SQLColumn<OlympaPlayerPvPKit>("level", "SMALLINT UNSIGNED NULL DEFAULT 1", Types.SMALLINT).setUpdatable();
 	private static final SQLColumn<OlympaPlayerPvPKit> COLUMN_XP = new SQLColumn<OlympaPlayerPvPKit>("xp", "INTEGER UNSIGNED NULL DEFAULT 0", Types.INTEGER).setUpdatable();
 	private static final SQLColumn<OlympaPlayerPvPKit> COLUMN_KILLS = new SQLColumn<OlympaPlayerPvPKit>("kills", "INTEGER UNSIGNED NULL DEFAULT 0", Types.INTEGER).setUpdatable();
-	
+
 	public static final List<SQLColumn<OlympaPlayerPvPKit>> COLUMNS = Arrays.asList(COLUMN_MONEY, COLUMN_KILL_STREAK, COLUMN_KILL_STREAK_MAX, COLUMN_LEVEL, COLUMN_XP, COLUMN_KILLS);
-	
+
 	private OlympaMoney money = new OlympaMoney(0);
 	private ObservableInt killStreak = new ObservableInt(0);
 	private ObservableInt killStreakMax = new ObservableInt(0);
 	private ObservableInt level = new ObservableInt(1);
 	private ObservableInt xp = new ObservableInt(0);
 	private ObservableInt kills = new ObservableInt(0);
-	
+
 	private Kit usedKit = null;
-	
+
 	public OlympaPlayerPvPKit(UUID uuid, String name, String ip) {
 		super(uuid, name, ip);
 	}
-	
+
 	@Override
 	public void loaded() {
 		money.observe("datas", () -> COLUMN_MONEY.updateAsync(this, money.get(), null, null));
@@ -51,7 +51,8 @@ public class OlympaPlayerPvPKit extends OlympaPlayerObject implements MoneyPlaye
 		killStreak.observe("datas", () -> COLUMN_KILL_STREAK.updateAsync(this, killStreak.get(), null, null));
 		killStreak.observe("scoreboard_update", () -> OlympaPvPKit.getInstance().lineKillStreak.updateHolder(OlympaPvPKit.getInstance().scoreboards.getPlayerScoreboard(this)));
 		killStreak.observe("updateMax", () -> {
-			if (killStreak.get() > killStreakMax.get()) killStreakMax.set(killStreak.get());
+			if (killStreak.get() > killStreakMax.get())
+				killStreakMax.set(killStreak.get());
 		});
 		killStreakMax.observe("datas", () -> COLUMN_KILL_STREAK_MAX.updateAsync(this, killStreakMax.get(), null, null));
 		killStreakMax.observe("ranking", () -> OlympaPvPKit.getInstance().bestKSRank.handleNewScore(getName(), getPlayer(), killStreakMax.get()));
@@ -68,60 +69,61 @@ public class OlympaPlayerPvPKit extends OlympaPlayerObject implements MoneyPlaye
 		kills.observe("ranking", () -> OlympaPvPKit.getInstance().totalKillRank.handleNewScore(getName(), getPlayer(), kills.get()));
 		kills.observe("scoreboard_update", () -> OlympaPvPKit.getInstance().lineKills.updateHolder(OlympaPvPKit.getInstance().scoreboards.getPlayerScoreboard(this)));
 	}
-	
+
 	@Override
 	public OlympaMoney getGameMoney() {
 		return money;
 	}
-	
+
 	public ObservableInt getKillStreak() {
 		return killStreak;
 	}
-	
+
 	public int getKillStreakMax() {
 		return killStreakMax.get();
 	}
-	
+
 	public int getLevel() {
 		return level.get();
 	}
-	
+
 	public void setLevel(int level) {
 		this.level.set(Math.min(Math.max(level, 1), XPManagement.XP_PER_LEVEL.length - 1));
 	}
-	
+
 	public int getXP() {
 		return xp.get();
 	}
-	
+
 	public void setXP(int xp) {
 		this.xp.set(Math.min(Math.max(xp, 0), Short.MAX_VALUE));
 	}
-	
+
 	public void updateXPBar() {
 		Player p = getPlayer();
 		p.setLevel(level.get());
 		float xpRatio = (float) xp.get() / (float) XPManagement.getXPToLevelUp(level.get());
-		if (xpRatio <= 1) p.setExp(xpRatio);
+		if (xpRatio <= 1)
+			p.setExp(xpRatio);
 	}
-	
+
 	public ObservableInt getKills() {
 		return kills;
 	}
-	
+
 	public boolean isInPvPZone() {
 		return usedKit != null;
 	}
-	
+
 	public Kit getUsedKit() {
 		return usedKit;
 	}
-	
+
 	public void setInPvPZone(Kit usedKit) {
 		this.usedKit = usedKit;
 		OlympaPvPKit.getInstance().lineKit.updateHolder(OlympaPvPKit.getInstance().scoreboards.getPlayerScoreboard(this));
 	}
-	
+
 	@Override
 	public void loadDatas(ResultSet resultSet) throws SQLException {
 		money.set(resultSet.getDouble("money"));
@@ -132,9 +134,9 @@ public class OlympaPlayerPvPKit extends OlympaPlayerObject implements MoneyPlaye
 		kills.set(resultSet.getInt("kills"));
 		updateXPBar();
 	}
-	
+
 	public static OlympaPlayerPvPKit get(Player p) {
-		return AccountProvider.getter().get(p.getUniqueId());
+		return AccountProviderAPI.getter().get(p.getUniqueId());
 	}
-	
+
 }
